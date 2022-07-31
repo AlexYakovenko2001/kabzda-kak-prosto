d2c088f415e9eb6925b1e4c00e6e9ae8e1ac2848
AlexYakovenko2001 <mrberrington2001@gmail.com>
2022-07-31 13:11:36 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
+    console.log('App is rendering')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+       <Accordion title={'Menu'} collapsed={true}/>
+       <Accordion title={'Users'} collapsed={false}/>
+
+       <Rating value={1}/>
+       <Rating value={2}/>
+       <Rating value={3}/>
+       <Rating value={4}/>
+       <Rating value={5}/>
+   </>);
 }
+type PageTitlePropsType = {
+    title: string
+}
+function PageTitle(props: PageTitlePropsType) {
+    console.log('PageTitle is rendering')
+    return <h1> {props.title} </h1>
+}
+
 
 export default App;
