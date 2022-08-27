e9c85432baa83d4effcb7a8b7dabf06bbd010aa8
AlexYakovenko2001 <mrberrington2001@gmail.com>
2022-08-27 20:27:21 +0300

    add array of items to Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {ControlledInput} from './components/input/ControlledInput/ControlledInput';
-import {ControlledCheckbox} from './components/input/ControlledCheckbox/ControlledCheckbox';
-import {ControlledSelect} from './components/input/ControlledSelect/ControlledSelect';
+import Accordion from './components/Accordion/Accordion';
 
 function App() {
-
+    const [collapsed, setCollapsed] = useState(true)
+    const onClick = (value: any) => {
+        console.log(`item ${value} was clicked`)
+    }
     return (
         <div className={'App'}>
-            <ControlledInput/>
-            <br/>
-            <br/>
-            <ControlledCheckbox/>
-            <br/>
-            <br/>
-            <ControlledSelect/>
+        <Accordion title={'Users'} collapsed={collapsed} onClickCallBack={setCollapsed} items={[{title:'Alexander', value: 1}, {title:'Viktor', value: 2}, {title:'Alexey', value: 3}, {title:'Anatoliy', value: 4}]} onClick={onClick}/>
         </div>);
 }
 
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,40 +1,50 @@
-import React from "react";
+import React from 'react';
+
 type AccordionTitlePropsType = {
     title: string
     onClickCallBack: () => void
 }
+
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log('AccordionTitle is rendering');
-    return(
+    return (
         <h3 onClick={props.onClickCallBack}>{props.title}</h3>
     )
 }
-type AccordionBodyPropsType = {
 
+type AccordionBodyPropsType = {
+    items: ItemType[]
+    onClick: (value: any) => void
 }
+
 function AccordionBody(props: AccordionBodyPropsType) {
     console.log('AccordionBody is rendering');
-        return (
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
+    return <ul>
+        {props.items.map((i, index) => <li onClick={() => props.onClick(i.value)} key={index}>{i.title}</li>)}
+    </ul>
 
-            </ul>
-        )
-    }
-
+}
+type ItemType = {
+    title: string
+    value: any
+}
 type AccordionPropsType = {
     title: string,
     collapsed: boolean
     onClickCallBack: (value: boolean) => void
+    items: ItemType[]
+    onClick: (value: any) => void
 }
+
 function Accordion(props: AccordionPropsType) {
     console.log('Accordion is rendering');
     return (<>
-            <AccordionTitle title={props.title} onClickCallBack={ () => {props.onClickCallBack(!props.collapsed)} }/>
-            { !props.collapsed && <AccordionBody/>}
+            <AccordionTitle title={props.title} onClickCallBack={() => {
+                props.onClickCallBack(!props.collapsed)
+            }}/>
+            {!props.collapsed && <AccordionBody items={props.items} onClick={props.onClick}/>}
         </>
     )
 }
+
 export default Accordion
